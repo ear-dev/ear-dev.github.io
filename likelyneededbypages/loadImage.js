dd7f8c0470ba1a453e744a8dfe0c4f2f8d408d0d
Peter Lepeska <peter.lepeska@viasat.com>
2016-06-22 16:48:27 -0400

    enhancements

--- likelyneededbypages/loadImage.js
+++ likelyneededbypages/loadImage.js
@@ -3,7 +3,15 @@
 
     var imageNode = document.createElement('img');
     imageNode.src = 'sparrow.png?inviewport-jsloaded';
-    
+
     var inviewportDiv = document.getElementById('inviewport-images');
     inviewportDiv.appendChild(imageNode);
 })();
+
+function loadOutOfViewportImageLate(){
+  var imageNode = document.createElement('img');
+  imageNode.src = 'sparrow.png?outviewport-jsloaded';
+
+  var inviewportDiv = document.getElementById('not-inviewport-images');
+  inviewportDiv.appendChild(imageNode);
+}
