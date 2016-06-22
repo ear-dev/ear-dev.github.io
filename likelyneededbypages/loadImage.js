403fdfbdf85d5695ead78c6f22275fe60ffbbc98
Peter Lepeska <peter.lepeska@viasat.com>
2016-06-22 17:03:49 -0400

    enhancements

--- likelyneededbypages/loadImage.js
+++ likelyneededbypages/loadImage.js
@@ -8,10 +8,18 @@
     inviewportDiv.appendChild(imageNode);
 })();
 
-function loadOutOfViewportImageLate(){
+function loadStuffLate(){
+  // fetch an out of viewport image late
   var imageNode = document.createElement('img');
   imageNode.src = 'sparrow.png?outviewport-jsloaded';
 
   var inviewportDiv = document.getElementById('not-inviewport-images');
   inviewportDiv.appendChild(imageNode);
+
+  //Fetch a script late
+  var scrptE = document.createElement("script");
+  scrptE.setAttribute("type", "text/javascript");
+  scrptE.setAttribute("language", "JavaScript");
+  scrptE.setAttribute("src", "doesNothing.js?secondTime");
+  document.getElementsByTagName("head")[0].appendChild(scrptE);
 }
